780ed9a867e41a293cecc73fa94ec1d1784eb1b8
Jose Carlos <48320837+Jebyto@users.noreply.github.com>
2026-06-17 15:04:05 -0300

    feat: Criação das strings em Português

---
+++ plugins/blocks/pomodoro/lang/pt_br/block_pomodoro.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings em português para o bloco Pomodoro.
+ *
+ * @package   block_pomodoro
+ * @copyright 2026 José Carlos
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Pomodoro';
+$string['pomodoro:addinstance'] = 'Adicionar bloco Pomodoro';
+$string['pomodoro:myaddinstance'] = 'Adicionar bloco Pomodoro ao Painel';
+$string['title'] = 'Timer Pomodoro';
+$string['subtitle'] = 'Inicie uma sessão de foco e acompanhe o timer pelas páginas do Moodle.';
+$string['summary'] = 'Sessão atual';
+$string['startpomodoro'] = 'Iniciar Pomodoro';
+$string['openfullscreen'] = 'Abrir tela cheia';
